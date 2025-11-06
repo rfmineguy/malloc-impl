9409629a2a8532e14210e81833489e1bb8cabbc4
Riley Fischer <rfmineguy@gmail.com>
2025-11-06 00:02:32 -0800

    make it so that freed regions are able to be remalloced if the size is enough

--- main.c
+++ main.c
@@ -15,41 +15,72 @@ void heap_dump();
 
 int main() {
   memset(heap, 0, HEAP_SIZE);
+  *(uint32_t*)(heap) = HEAP_SIZE - 5;
+  *(uint32_t*)(heap + 4) = 0;
 
   void* x = mymalloc(0x5);
   *(uint32_t*)x = 0x56f39b1a;
   *(uint8_t*)(x+4) = 0x44;
 
-  void* y = mymalloc(0x20);
+  void* y = mymalloc(0x4);
   *(uint32_t*)y = 0x13f4b5a3;
 
-  void* z = mymalloc(0x10);
+  void* z = mymalloc(0x6);
   *(uint16_t*)z = 0x13f4;
+  *(uint16_t*)(z + 1) = 0x2405;
 
   myfree(y);
-  y = mymalloc(0x10);
+  y = mymalloc(0x5);
   *(uint32_t*)y = 0x8523fd13;
+
+  void* d = mymalloc(0x4);
+  *(uint32_t*)d = 0xdddddddd;
+
+  // NOTE:
+  // in the current state, allocation y ends up overwriting memory in z
+  // due to not checking if there is enough space for y in the newly freed region
+  // it was 4 bytes long, but we ask for 10 bytes. 10 bytes cant fit in 4 bytes
 
   heap_dump();
 }
 
 void* mymalloc(uint32_t size) {
+  printf("=========== mymalloc ===========\n");
   int offset = 0;
-  uint32_t stride = ((uint32_t*)(offset + heap))[0];
-  uint8_t  flags  = ((uint8_t* )(offset + heap + 4))[0];
+  // uint32_t stride = ((uint32_t*)(offset + heap))[0];
+  // uint8_t  flags  = ((uint8_t* )(offset + heap + 4))[0];
 
   // while we see regions that are marked as used, go
   // further into the heap
-  while ((flags & FLAG_USED) == FLAG_USED) {
-    offset += sizeof(uint32_t) + sizeof(uint8_t) + stride;
-    stride = ((uint32_t*)(offset + heap))[0];
-    flags  = ((uint8_t* )(offset + heap + 4))[0];
+  // skip regions that are too small as well
+  while (offset < HEAP_SIZE) {
+    uint32_t stride = ((uint32_t*)(offset + heap))[0];
+    uint8_t flags   = ((uint8_t* )(offset + heap + 4))[0];
+
+    if ((flags & FLAG_USED) == FLAG_USED) {
+      offset += 5 + stride;
+      continue;
+    }
+    if (stride < size) {
+      offset += 5 + stride;
+      continue;
+    }
+    break;
   }
 
+  if (offset >= HEAP_SIZE) return NULL;
   // when we finally find an unused region
   // mark it as used, and fill in the size
+  uint32_t oldsize = *(uint32_t*)(heap + offset);
   *(uint32_t*)(heap + offset) = size;
   *(uint8_t*)(heap + offset + 4) |= FLAG_USED;
+
+  uint32_t newoff = offset + 5 + size;
+  uint32_t newsize = oldsize - size - 5;
+  if (newoff + 5 <= HEAP_SIZE) {
+    *(uint32_t*)(heap + newoff) = newsize;
+    *(uint8_t*)(heap + newoff + 4) = 0;
+  }
 
   return heap + offset + 5;
 }

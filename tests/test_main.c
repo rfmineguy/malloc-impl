78ebc7a3146b45787d5d3c1c6a9fcf58f536bbee
Riley Fischer <rfmineguy@gmail.com>
2025-11-08 21:58:16 -0800

    test: write unit test to make sure the heap gets initialized correctly

--- tests/test_main.c
+++ tests/test_main.c
@@ -20,6 +20,24 @@ int main(int argc, char** argv) {
 }
 
 MunitResult test_heap_init(const MunitParameter params[], void* user_data_or_fixture) {
+  heap_init();
+  uint8_t* heap = heap_test_get();
+
+  // 1. ensure the entire heap is zeroed other than the:
+  //       header
+  //       trailer
+  //       flags
+  for (int i = 5; i < HEAP_SIZE - 5; i++)
+    munit_assert_int(heap[i], ==, 0);
+
+  // 2. check the header is appropriate for the HEAP_SIZE
+  munit_assert_int(*(uint32_t*)heap, ==, HEAP_SIZE - 9);
+
+  // 3. check the flags are not set
+  munit_assert_int(*(uint8_t*)(heap + 4), ==, 0x0);
+
+  // 4. check the trailer is equal to the header
+  munit_assert_int(*(uint32_t*)(heap + HEAP_SIZE - 5), ==, HEAP_SIZE - 9);
   return MUNIT_OK;
 }
 

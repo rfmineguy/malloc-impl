bc3b03060c08df622122df2e4866a2cfe0812a12
Riley Fischer <rfmineguy@gmail.com>
2025-11-09 08:51:02 -0800

    test: refactor alternate test to use the factored out test_util

--- tests/main.c
+++ tests/main.c
@@ -1,4 +1,5 @@
 #include "../mymalloc.h"
+#include "test_util.h"
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
@@ -13,24 +14,8 @@ int test_rng_seeds[TEST_RNG_SEEDS_COUNT] = {
   0x4b82f5b
 };
 
-typedef struct {
-  int size;
-  void *ptr;
-} test_allocation;
-
-void shuffle_array(test_allocation array[], size_t count) {
-  for (int i = 0; i < count; i++) {
-    int i = rand() % count;
-    int j = rand() % count;
-
-    test_allocation a = array[i];
-    array[i] = array[j];
-    array[j] = a;
-  }
-}
-
 void test_malloc_free() {
-  test_allocation allocations[50] = {0};
+  test_util_allocation allocations[50] = {0};
   heap_init();
 
   for (int i = 0; i < 50; i++) {
